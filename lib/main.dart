5c6468b523863005914b2ac9319b90577fe9baaf
Mohamed Ayman <mousa2692004@gmail.com>
2026-01-21 16:21:37 +0200

    Add onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sijil_patient_portal/core/cache/shared_prefs_utils.dart';
 import 'package:sijil_patient_portal/core/utils/app_routes.dart';
 import 'package:sijil_patient_portal/core/utils/app_theme.dart';
 import 'package:sijil_patient_portal/features/auth/sign_in/screen/sign_in_screen.dart';
@@ -7,17 +9,22 @@ import 'package:sijil_patient_portal/features/home/screen/home_screen.dart';
 import 'package:sijil_patient_portal/features/onboarding/screen/onboarding_screen.dart';
 
 void main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPrefsUtils.sharedPrefs = await SharedPreferences.getInstance();
+  final token = SharedPrefsUtils.getData(key: "onboarding");
+  runApp(MyApp(token: token));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  final token;
+  const MyApp({super.key, required this.token});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.homeScreen,
+      initialRoute: token == true
+          ? AppRoutes.signUpScreen
+          : AppRoutes.onboardingsScreen,
       routes: {
         AppRoutes.homeScreen: (context) => HomeScreen(),
         AppRoutes.signInScreen: (context) => SignInScreen(),
