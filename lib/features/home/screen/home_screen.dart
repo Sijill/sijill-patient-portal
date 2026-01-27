8ec1962d20f0ec677c821a640a067ffc5f429313
muunirosama <osamamunir323@gmail.com>
2026-01-27 17:39:30 +0200

    Add comment in home_screen.dart

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -7,5 +7,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(backgroundColor: AppColors.primaryColor));
+    //munir
   }
 }
